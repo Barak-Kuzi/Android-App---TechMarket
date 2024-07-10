9a50eb56a97a671e8d946af6ffa90bb11feea396
Barak-Kuzi <barakkuzi1997@gmail.com>
2024-07-11 01:22:01 +0300

    Main Layout

---
+++ app/src/main/java/com/example/techmarket_finalproject/Activity/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.techmarket_finalproject.Activity;
+
+import android.os.Bundle;
+import android.view.Window;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.example.techmarket_finalproject.Adapter.PopularAdapter;
+import com.example.techmarket_finalproject.R;
+import com.example.techmarket_finalproject.databinding.ActivityMainBinding;
+import com.example.techmarket_finalproject.domain.PopularDomain;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding activityMainBinding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(activityMainBinding.getRoot());
+
+        statusBarColor();
+        initRecyclerView();
+    }
+
+    private void statusBarColor() {
+        Window window = MainActivity.this.getWindow();
+        window.setStatusBarColor(ContextCompat.getColor(MainActivity.this, R.color.purple_Dark));
+    }
+
+    private void initRecyclerView() {
+        ArrayList<PopularDomain> items = new ArrayList<>();
+        items.add(new PopularDomain("T-shirt black", "item_1", 15, 4, 500, "testttttttttttttttttttttttt"));
+        items.add(new PopularDomain("Smart Watch", "item_2", 10, 4.5, 450, "testttttttttttttttttttttttt"));
+        items.add(new PopularDomain("Phone", "item_3", 3, 4.9, 800, "testttttttttttttttttttttttt"));
+
+        activityMainBinding.popularView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+        activityMainBinding.popularView.setAdapter(new PopularAdapter(items));
+    }
+}
---
+++ app/src/main/java/com/example/techmarket_finalproject/domain/PopularDomain.java
@@ -0,0 +1,76 @@
+package com.example.techmarket_finalproject.domain;
+
+public class PopularDomain {
+    private String title;
+    private String imageUrl;
+    private int review;
+    private double score;
+    private int numberInChart;
+    private double price;
+    private String description;
+
+    public PopularDomain(String title, String imageUrl, int review, double score, double price, String description) {
+        this.title = title;
+        this.imageUrl = imageUrl;
+        this.review = review;
+        this.score = score;
+        this.price = price;
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public int getReview() {
+        return review;
+    }
+
+    public void setReview(int review) {
+        this.review = review;
+    }
+
+    public double getScore() {
+        return score;
+    }
+
+    public void setScore(double score) {
+        this.score = score;
+    }
+
+    public int getNumberInChart() {
+        return numberInChart;
+    }
+
+    public void setNumberInChart(int numberInChart) {
+        this.numberInChart = numberInChart;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
